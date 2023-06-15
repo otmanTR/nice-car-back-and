64262161c5b47b355c4371b1554ffcae8443adde
Becky449 <rabnab12@gmail.com>
2023-06-15 17:25:13 +0300

    Created reservations table and added its validations

---
+++ app/models/reservation.rb
@@ -0,0 +1,8 @@
+class Reservation < ApplicationRecord
+  belongs_to :car, foreign_key: 'car_id'
+  belongs_to :user, foreign_key: 'user_id'
+
+  validates :start_date, :end_date, :city, presence: true
+  validate :start_date_cannot_be_in_the_past, :end_date_cannot_be_before_start_date
+  
+end
