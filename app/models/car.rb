0bb3bfd60e8c5b5088d50522df13e47ccec6dfc1
Joseph David <jodavid444dave@gmail.com>
2023-06-17 13:48:32 +0100

    Fix linter errors

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,12 +1,11 @@
 class Car < ApplicationRecord
- validates :name, presence: true, length: { minimum: 3, message: 'Name must have 3 or more characters' }
- validates :image, presence: true,
- validates :model, presence: true,
- validates :price,
+  validates :name, presence: true, length: { minimum: 3, message: 'Name must have 3 or more characters' }
+  validates :image, presence: true
+  validates :model, presence: true
+  validates :price,
             presence: true,
             numericality: { greater_than: 0, message: 'Price must be greater than 0' }
 
-belongs_to :user
-has_one :reservation, dependent: :destroy
-
+  belongs_to :user
+  has_one :reservation, dependent: :destroy
 end
