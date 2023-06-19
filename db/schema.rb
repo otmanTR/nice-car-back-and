f6d46545856d940ac33ed906fc31eb41d7903d35
otmanTR <r.brk.otman@gmail.com>
2023-06-19 13:46:11 +0300

    Add email to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_19_092703) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_19_104513) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,6 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_19_092703) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
   add_foreign_key "cars", "users"
