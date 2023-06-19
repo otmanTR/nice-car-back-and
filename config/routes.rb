f7057604ffbc100cd70e20e133add544ae35dc87
otmanTR <r.brk.otman@gmail.com>
2023-06-19 12:36:21 +0300

    Update cors

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :cars
-  resources :users
   
-  resources :reservations, only: [:index, :create, :destroy]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users  
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :reservations, only: [:index, :create, :destroy]
+      resources :cars, only: [:index, :create, :destroy]
+    end
+  end
 end
