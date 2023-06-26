68bc403fa6b5f1ce020bacf978cfe9d999963c91
Joseph David <jodavid.444dave@gmail.com>
2023-06-26 18:24:04 +0100

    Update route.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
       resources :users, only: [:index, :create] 
       post 'users/login', to: 'users#login'
       resources :reservations, only: [:index, :create, :destroy]
-      resources :cars, only: [:index, :create, :destroy]
+      resources :cars, only: [:index, :update, :create, :destroy]
     end
   end
 end
